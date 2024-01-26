c952d0f752676fc82cc6845bc544f81f9693c9a7
Matheus <mat.araujoribeiro@gmail.com>
2024-01-26 13:27:05 -0300

    db mapping

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿internal class Program
+﻿using BlogEF.Data;
+using BlogEF.Models;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
+        var ctx = new BlogEFDataContext();
+        
     }
 }
