f3147472af27bb5fdb8abbabcb480424844f54ef
Matheus <mat.araujoribeiro@gmail.com>
2024-01-28 18:05:29 -0300

    category fluentmapping

--- Data/Mappings/CategoryMap.cs
+++ Data/Mappings/CategoryMap.cs
@@ -8,9 +8,32 @@ public class CategoryMap : IEntityTypeConfiguration<Category>
 {
     public void Configure(EntityTypeBuilder<Category> builder)
     {
-        //Tabela
+        // Tabela
         builder.ToTable("Category");
 
+        // Chave Primária
+        builder.HasKey(x=>x.Id);
+
+        // Identity
+        builder.Property(x=>x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
         
+        // Propriedades
+        builder.Property(x=>x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+
+        builder.Property(x=>x.Slug)
+            .IsRequired()
+            .HasColumnName("Slug")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+
+        // Índices
+        builder.HasIndex(x=>x.Slug, "IX_Category_Slug")
+            .IsUnique();
     }
 }
