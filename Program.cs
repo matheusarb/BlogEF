2c6b2a057187a447bf69de2d6e353588753afa89
Matheus <mat.araujoribeiro@gmail.com>
2024-01-29 22:31:10 -0300

    async Task Main

--- Program.cs
+++ Program.cs
@@ -6,21 +6,49 @@ using Microsoft.VisualBasic;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        var user = new User
-        {
-            Name="teste",
-            Email="teste@",
-            PasswordHash="teste@",
-            Bio="teste@",
-            Image="teste@",
-            Slug="teste@",
-            Github="teste@"
-        };
-
         using var context = new BlogEFDataContext();
-        context.Users.Add(user);
-        context.SaveChanges();
+        
+        var usersTask = await GetUsers(context);
+        // var users = await usersTask;
+        DisplayUsers(usersTask);
+    }
+
+    static void DisplayUsers(IList<User> users)
+    {
+        foreach(var user in users)
+        {
+            System.Console.WriteLine(user.Name);
+        }
+    }
+
+    public static async Task<IList<User>> GetUsers(BlogEFDataContext context, int skip = 0, int take = 25)
+    {
+        return await context
+            .Users
+            .AsNoTracking()
+            .Skip(skip)
+            .Take(take)
+            .ToListAsync();
     }
 }
+
+// var user = new User
+        // {
+        //     Name="teste",
+        //     Email="teste@",
+        //     PasswordHash="teste@",
+        //     Bio="teste@",
+        //     Image="teste@",
+        //     Slug="teste@",
+        //     Github="teste@"
+        // };
+
+        // context.Users.Add(user);
+        // context.SaveChanges();
+        // var posts2 = await GetPosts(context);
+        // foreach (var item in posts2)
+        // {
+        //     System.Console.WriteLine(item.Slug);
+        // }
